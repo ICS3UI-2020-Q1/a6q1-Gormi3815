821cec8053c42c237be4d779ce2496e89fc87553
Gormi3815 <gormi3815@wrdsb.ca>
2020-10-09 12:34:04 +0000

    the program will now give the average height + the heights above average

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
-/**
+import java.util.Scanner;
+/** this program will take the heights provided and give the average + the heights that are above average
  *
- * @author 
+ * @author Isaac
  */
 public class Main {
 
@@ -10,6 +11,27 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+    System.out.println("How many heights would you like to put in?");
+    final int num = input.nextInt();
+    double[] height = new double[num];
+    for(int i= 0; i < height.length; i++){
+      System.out.println("Please enter in your heights " + (i + 1));
+      height[i] = input.nextDouble();
+    }
+    double sum = 0;
+    for(int i = 0; i<height.length; i++){
+      sum = sum + height[i];
+    }
+    double average = sum / num;
+    System.out.println("The average height is " + average);
+   for(int i = 0; i < height.length; i++){
+     if(height[i] > average){
     
+     
+     System.out.println("The heights above average are " + height[i]);
+     }
+   }
+    }
   }
-}
+
